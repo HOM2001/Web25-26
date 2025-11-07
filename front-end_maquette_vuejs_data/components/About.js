3e0fc80942318bbe052c40ada0c64701a500f008
Owaiss <hamidowaiss2001@gmail.com>
2025-11-07 11:03:13 +0100

    Modification about + ajout css

--- front-end_maquette_vuejs_data/components/About.js
+++ front-end_maquette_vuejs_data/components/About.js
@@ -1,32 +1,37 @@
-
-
 export default {
-    name: 'About', // Nom du composant
+    name: 'About',
 
-    template : `   
-          <main  >
-            <h1>Livrable "front-end"</h1>
-            <ul>
-                <li>
-                   Page d'accueil : 10 articles disponibles (les 10 premiers ) <br>
-                   Cliquer sur bouton lire plus pour déployer l'article et fermer pour le fermer
-                </li>      
-                <br>      
-                <li>
-               Page d'accueil : Liste de tous les articles disponibles ( 25 ) sur 2 colonnes <br>
-               Cliquer sur le cadre pour lire l'article.
-               </li>
-               <br>
-               <li>
-               Page recherche : permet de rechercher des articles par mot et catégorie ( pour l'instant titre et more 
-               </li>
-               <br>
-               Page de favoris : Tableau avec les titres , auteurs , lecture , catégorie et résumé des articles et un checkbox pour cocher et l'ajouter au favorite <br>
-               Quand on clique sur la checkbox un panier apparait avec l'article en favoris 
-            </ul>
-          </main>
-    `,
+    template: `
+    <main class="about-page">
+      <h1 class="about-title">Livrable "front-end"</h1>
+      <ul class="about-list">
+        <li class="about-item">
+          <p>
+            <strong>Page d'accueil :</strong> 10 articles disponibles (les 10 premiers)<br>
+            Cliquer sur le bouton "Lire plus" pour déployer l'article et "Fermer" pour le refermer.
+          </p>
+        </li>
 
+        <li class="about-item">
+          <p>
+            <strong>Page d'accueil :</strong> Liste de tous les articles disponibles (25) sur 2 colonnes<br>
+            Cliquer sur le cadre pour lire l'article.
+          </p>
+        </li>
 
+        <li class="about-item">
+          <p>
+            <strong>Page recherche :</strong> permet de rechercher des articles par mot et catégorie (pour l'instant : titre et more).
+          </p>
+        </li>
+
+        <li class="about-item">
+          <p>
+            <strong>Page de favoris :</strong> Tableau avec les titres, auteurs, lecture, catégorie et résumé des articles, et un checkbox pour cocher et l'ajouter aux favoris.<br>
+            Quand on clique sur la checkbox, un panier apparaît avec l'article en favoris.
+          </p>
+        </li>
+      </ul>
+    </main>
+  `
 }
-
