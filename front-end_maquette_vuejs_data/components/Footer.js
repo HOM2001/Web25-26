826d8904600988484ccdf4c4b83aa48523f22034
TheFondatrice <imane.amane16@gmail.com>
2025-11-09 21:08:04 +0100

    ajout bootstrap 5 + mise en place header & footer + ajout logo

--- front-end_maquette_vuejs_data/components/Footer.js
+++ front-end_maquette_vuejs_data/components/Footer.js
@@ -4,8 +4,15 @@ export default {
     name: 'Footer', // Nom du composant
 
     template : `   
-          <footer class="footer">
-            <p>Grouge BootsTrappers : HAMID Owaiss / Amane Imane</p>
-          </footer>
+          <div class="container">
+              <footer class="align-items-center py-3 my-4 border-top">
+                  <div class="">
+                        <a aria-current="page" href="#" @click.prevent="$parent.showPage('home')">
+                          <img src="media/Last news.png" alt="Last news" width="60" height="48">
+                        </a>
+                        <span class="text-body-secondary">© Groupe BootsTrappers : HAMID Owaiss / Amane Imane</span>
+                  </div>
+              </footer>
+          </div>
     `,
 }
--- front-end_maquette_vuejs_data/components/Menu.js
+++ front-end_maquette_vuejs_data/components/Menu.js
@@ -34,24 +34,35 @@ export default {
     },
     template : ` 
  <!-- Menu de navigation-->
-  <main class="menu-main" :style="{ color: fontColor, border: borderStyle }">
-        <nav>
-          <ul>
-           <li><a href="#" @click.prevent="$parent.showPage('home')">Accueil</a></li>  |
-            <li><a href="#" @click.prevent="$parent.showPage('articles')">Article</a></li> |
-            <li><a href="#" @click.prevent="$parent.showPage('search')">Recherche</a></li>|
-            <li><a href="#" @click.prevent="$parent.showPage('favorite')">Favoris</a></li>|
-            <li><a href="#" @click.prevent="$parent.showPage('about')">À propos</a></li>|
-            <li><a href="#" @click.prevent="$parent.showPage('login')">Log in</a></li>
-            <!-- Afficher nom use connecté -->
-            <li v-if="user" class="user-name">
-        {{ user }}
-        </li>
-        <!-- Bouton de déconnection apres login  -->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+
+        <a class="navbar-brand" href="#">
+          <img src="media/Last news.png" alt="Last news" width="120" height="96">
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true"    aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="navbar-collapse collapse show" id="navbarNav" style="">
+          <ul class="navbar-nav">
+          <li class="nav-item"><a class="nav-link active" aria-current="page" href="#" @click.prevent="$parent.showPage('home')">Accueil</a></li>
+          <li class="nav-item"><a class="nav-link active" aria-current="page" href="#" @click.prevent="$parent.showPage('articles')">Article</a></li>
+          <li class="nav-item"><a class="nav-link active" aria-current="page" href="#" @click.prevent="$parent.showPage('search')">Recherche</a></li>
+          <li class="nav-item"><a class="nav-link active" aria-current="page" href="#" @click.prevent="$parent.showPage('favorite')">Favoris</a></li>
+          <li class="nav-item"><a class="nav-link active" aria-current="page" href="#" @click.prevent="$parent.showPage('about')">À propos</a></li>
+          <li class="nav-item"><a class="nav-link active" aria-current="page" href="#" @click.prevent="$parent.showPage('login')">Log in</a></li>
+          <!-- Afficher nom use connecté -->
+          <li class="nav-item" v-if="user" class="user-name">
+          {{ user }}
+          </li>
+          <!-- Bouton de déconnection apres login  -->
           <button v-if="user"  @click="logout" class="logout-btn">Déconnexion</button>
-         </ul>
-        </nav>
-        </main>
+          </ul>
+        </div>
+      </div>
+    </nav>
         <!-- Option de présentations de la couleur de la police  -->
   <label>
     Choisir couleur de la police :
